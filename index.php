d7516582e2628c65e1b4f817931324197510b3f3
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-16 11:41:57 -0700

    watched milestone 2 & 3 coded

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kathlyn's To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="warp">
+
+  </div>
+</body>
+</html>
